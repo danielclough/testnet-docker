53a4fa448696a2ccf058684ad07dc2dd01eed6cc
danielclough <danielclough@gmail.com>
2022-03-24 00:46:00 +0100

    more complete docker-compose install script

--- testnet-setup.sh
+++ testnet-setup.sh
@@ -9,6 +9,11 @@ composeInstalled=`docker-compose -v`
 if [ -n "$var" ]; then
     echo "docker-compose not installed - installing now"
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    sudo curl \
+        -L https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/bash/docker-compose \
+        -o /etc/bash_completion.d/docker-compose
 else
     echo -e "docker-compose installed - moving on \n"
 fi
